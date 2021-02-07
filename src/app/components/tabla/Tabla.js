d2dbc5244e2f4b131a6fcdccc1ea05edbe55faab
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-02-07 12:34:54 -0500

    Agregado botones para actualizar y eliminar

--- src/app/components/tabla/Tabla.js
+++ src/app/components/tabla/Tabla.js
@@ -9,6 +9,7 @@ return (
             <th scope="col">Nombre</th>
             <th scope="col">Apellido</th>
             <th scope="col">Email</th>
+            <th scope="col">Acción</th>
         </tr>
     </thead>
     <tbody>
@@ -17,19 +18,34 @@ return (
             <td>Mark</td>
             <td>Otto</td>
             <td>mark@mdo</td>
+            <td>
+                <button type="button" class="btn btn-primary">Actualizar</button>
+                <button type="button" class="btn btn-danger">Eliminar</button>
+            </td>
         </tr>
         <tr>
             <th scope="row">2</th>
             <td>Jacob</td>
             <td>Thornton</td>
             <td>jacob@fat</td>
+            <td>
+                <button type="button" class="btn btn-primary">Actualizar</button>
+                <button type="button" class="btn btn-danger">Eliminar</button>
+            </td>
         </tr>
         <tr>
             <th scope="row">3</th>
             <td>Marlon</td>
             <td>García</td>
             <td>marlon@fat</td>
+            <td>
+                <button type="button" class="btn btn-primary">Actualizar</button>
+                <button type="button" class="btn btn-danger">Eliminar</button>
+            </td>
         </tr>
+ 
+        
+
     </tbody>
 </table>
 )
