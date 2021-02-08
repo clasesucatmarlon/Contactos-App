304647248f4552f61d3c1520c7c6697e8b7d9f71
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-02-08 10:44:06 -0500

    Agregando iconos a los botones

--- src/app/components/tabla/Tabla.js
+++ src/app/components/tabla/Tabla.js
@@ -19,9 +19,13 @@ return (
             <td>Otto</td>
             <td>mark@mdo</td>
             <td>
-                <button type="button" class="btn btn-primary">Actualizar</button>
-                <button type="button" class="btn btn-danger">Eliminar</button>
-            </td>
+                <button type="button" className="btn btn-primary">
+                    <i className="bi bi-pencil"></i>
+                </button>
+                &nbsp;&nbsp;&nbsp;
+                <button type="button" className="btn btn-danger">
+                    <i className="bi bi-trash"></i>
+                </button>            </td>
         </tr>
         <tr>
             <th scope="row">2</th>
@@ -29,8 +33,13 @@ return (
             <td>Thornton</td>
             <td>jacob@fat</td>
             <td>
-                <button type="button" class="btn btn-primary">Actualizar</button>
-                <button type="button" class="btn btn-danger">Eliminar</button>
+                <button type="button" className="btn btn-primary">
+                    <i className="bi bi-pencil"></i>
+                </button>
+                &nbsp;&nbsp;&nbsp;
+                <button type="button" className="btn btn-danger">
+                    <i className="bi bi-trash"></i>
+                </button>
             </td>
         </tr>
         <tr>
@@ -39,11 +48,16 @@ return (
             <td>García</td>
             <td>marlon@fat</td>
             <td>
-                <button type="button" class="btn btn-primary">Actualizar</button>
-                <button type="button" class="btn btn-danger">Eliminar</button>
+                <button type="button" className="btn btn-primary">
+                    <i className="bi bi-pencil"></i>
+                </button>
+                &nbsp;&nbsp;&nbsp;
+                <button type="button" className="btn btn-danger">
+                    <i className="bi bi-trash"></i>
+                </button>
             </td>
         </tr>
- 
+
         
 
     </tbody>
