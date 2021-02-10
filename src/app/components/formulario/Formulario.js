bc80cd6577931e3ccd09648fac93af883862496f
clasesucatmarlon <clasesucatmarlon@gmail.com>
2021-02-09 21:12:48 -0500

    Limpiando formulario

--- src/app/components/formulario/Formulario.js
+++ src/app/components/formulario/Formulario.js
@@ -12,38 +12,38 @@ const handleSubmit = (event) => {
     const contacts = JSON.parse(localStorage.getItem('contacts')) || [];
 
     let contact = {};
-    console.log(formData)
     for (const [key, value] of formData.entries()) {
         contact[key] = value;
     }
 
-    /* contact.id = uuid(); */
+    contact.id = uuid();
 
-    console.log(contact)
+    contacts.push(contact);
 
-    contacts.push(contact)
+    localStorage.setItem("contacts", JSON.stringify(contacts));
 
-    localStorage.setItem("contacts", JSON.stringify(contacts))
+    console.log(event)
+    event.target.reset();
 
 };
 
 return (
 <form method="POST" className="bg-white rounded p-5" onSubmit={ handleSubmit }>
     <div className="mb-3">
-        <label htmlFor="name" className="form-label">Nombre:</label>
-        <input type="text" className="form-control" id="nombre" />
+        <label for="name" className="form-label">Nombre:</label>
+        <input type="text" name="name" className="form-control" id="nombre" />
     </div>
     <div className="mb-3">
-        <label htmlFor="last" className="form-label">Apellido:</label>
-        <input type="text" className="form-control" id="nombre" />
+        <label for="last" className="form-label">Apellido:</label>
+        <input type="text" name="" className="form-control" id="nombre" />
     </div>
     <div className="mb-3">
-        <label htmlFor="email" className="form-label">Email:</label>
-        <input type="email" className="form-control" id="email" />
+        <label for="email" className="form-label">Email:</label>
+        <input type="email" name="email" className="form-control" id="email" />
     </div>
     <div className="mb-3">
-        <label htmlFor="password" className="form-label">Password:</label>
-        <input type="password" className="form-control" id="password" />
+        <label for="password" className="form-label">Password:</label>
+        <input type="password" name="password" className="form-control" id="password" />
     </div>
 
     <button type="submit" value="submit" className="btn btn-primary">Agregar</button>
